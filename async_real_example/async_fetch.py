65d5c7d1b565edb0797494b5101d996d9ab97329
Stu Ryan <mikeryan56@gmail.com>
2019-02-10 14:50:35 -0500

    adding progress bar to sync and async calls with TQDM

--- async_real_example/async_fetch.py
+++ async_real_example/async_fetch.py
@@ -1,30 +1,45 @@
 import time
 import asyncio
+from colorama import init as color_init, Fore, Back, Style
+from tqdm import tqdm
 from aiohttp import ClientSession, TCPConnector
+
+# init colorama
+color_init()
 
 task_info = []
 
 
 
-async def fetch(url, session):
+async def fetch(url, session, url_count):
     async with session.get(url) as response:
-        # print(response.json())
+        c0 = f"start call {url_count}"
+        c1 = f"complete call {url_count}"
+        # print(Fore.RED + c0) # uncomment if you want to watch calls
+        r = await response.json()
+        # print(Fore.YELLOW + c1) # uncomment if you want to watch calls
+        return r
 
-        return await response.json()
 
 async def run(r):
     tasks = []
-
+    url_count = 1
     # Fetch all responses within one Client session,
     # keep connection alive for all requests.
     # for my test 150 was the optimal number
-    conn = TCPConnector(limit=150)
+    conn = TCPConnector(limit=300)
     async with ClientSession(connector=conn) as session:
         for i in r:
-            task = asyncio.ensure_future(fetch(i, session))
+            task = asyncio.ensure_future(fetch(i, session,url_count))
             tasks.append(task)
+            url_count += 1
+
+        bob = []
+        for f in tqdm(asyncio.as_completed(tasks), total=len(tasks), desc='Async Calls',unit=' request'):
+            bob.append(await f)
 
         responses = await asyncio.gather(*tasks)
+
         # you now have all response bodies in this variable
         # print_responses(responses)
         return responses
@@ -41,7 +56,7 @@ def async_fetch(task_url):
     loop = asyncio.get_event_loop()
     future = asyncio.ensure_future(run(task_url))
     obj = loop.run_until_complete(future)
-    loop.close()
+    # loop.close()
     t1 = time.time() - t0
     # print(int(t1))
 
--- async_real_example/run.py
+++ async_real_example/run.py
@@ -1,6 +1,8 @@
 import time
 import requests
-from async_real_example.async_fetch import async_fetch
+from tqdm import tqdm, trange
+#from async_real_example.async_fetch import async_fetch
+from async_fetch import async_fetch
 
 def get_task_list(url):
     task_list = []
@@ -15,7 +17,7 @@ def get_task_list(url):
             id = f"{url}{i['id']}"
 
             # loop through to extend the amount of calls
-            for b in range(0, 100):
+            for b in range(0, 1000):
                 task_list.append(id)
 
     return task_list
@@ -23,45 +25,56 @@ def get_task_list(url):
 def sync_test(task_url):
     result = []
     c = 1
-    for e in task_url:
-
+    for e in tqdm(task_url, desc='Sync Calls',unit=' request', total=len(task_url)):
         r = requests.get(e)
-        print(c)
+        # print(c)
         c += 1
         result.append(r.json())
 
     return result
 
 def main():
+    # Change this to set a limit on how many Sync calls to make during the test.
+    sync_test_threshold = 100
     # start the clock
     t_start = time.time()
     # call API
     url = "https://learning-camunda.devsetgo.com/rest/engine/default/task/"
     task_url = get_task_list(url)
-    print(len(task_url))
+    # print(f"There are {len(task_url)} tasks to fetch")
+    if len(task_url) < sync_test_threshold:
 
-    # call async_1
-    print(f"There are {len(task_url)} tasks to be called.")
-    # t_start_sync = time.time()
-    # print(f"Starting sync test")
-    # result_sync = sync_test(task_url)
-    # t_duration_sync = time.time() - t_start_sync
-    # rate_sync = len(task_url) / t_duration_sync
-    # print(f'The sync process took {t_duration_sync} seconds to complete at a rate of ~{rate_sync} calls per seconds.')
+        # call async_1
+        print(f"There are {len(task_url)} tasks to fetch")
+        # print(f"There are {len(task_url)} tasks to be called.")
+        t_start_sync = time.time()
+        print(f"Starting sync test")
+        result_sync = sync_test(task_url)
+        t_duration_sync = time.time() - t_start_sync
+        rate_sync = len(task_url) / t_duration_sync
+        print(f'The sync process took {t_duration_sync:.2f} seconds to complete at a rate of ~{rate_sync:.2f} calls per seconds.')
 
-    t_start_async = time.time()
-    print(f"Starting async test")
-    result_async = async_fetch(task_url)
-    t_duration_async = time.time() - t_start_async
-    rate_async = len(task_url) / t_duration_async
-    print(f'The async process took {t_duration_async} seconds to complete at a rate of {rate_async} calls per seconds.')
-
-    # duration calc
-    t_duration = time.time() - t_start
-    print(f'The entire test took {t_duration} seconds to complete')
-    # t_faster = t_duration_sync - t_duration_async
-    # print(f"How much faster is Async? {t_faster} seconds or {t_duration_sync / t_duration_async} times faster and {rate_async - rate_sync} calls per second faster")
-
+        t_start_async = time.time()
+        print(f"Starting async test")
+        result_async = async_fetch(task_url)
+        t_duration_async = time.time() - t_start_async
+        rate_async = len(task_url) / t_duration_async
+        print(f'The async process took {t_duration_async:.2f} seconds to complete at a rate of {rate_async:.2f} calls per seconds.')
+        print(len(result_async))
+        # duration calc
+        t_duration = time.time() - t_start
+        print(f'The entire test took {t_duration:.2f} seconds to complete')
+        t_faster = t_duration_sync - t_duration_async
+        print(f"How much faster is Async? {t_faster:.2f} seconds or {(t_duration_sync / t_duration_async):.2f} times faster and {(rate_async - rate_sync):.2f} calls per second faster")
+    else:
+        print(f"Over Test Threshold of {sync_test_threshold} and will only proceed wth Async Test and fetch {len(task_url)} tasks")
+        t_start_async = time.time()
+        print(f"Starting async test")
+        result_async = async_fetch(task_url)
+        t_duration_async = time.time() - t_start_async
+        rate_async = len(task_url) / t_duration_async
+        print(f'The async process took {t_duration_async:.2f} seconds to complete at a rate of {rate_async:.2f} calls per seconds.')
+        # print(f"Process Results = {len(result_async)}")
     return
 
 
